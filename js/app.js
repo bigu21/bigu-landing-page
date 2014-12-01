53bc14e057e338011b745a3ab3d9aab5836847d8
Gabriel Almeida <gabriel.almeida.rj@gmail.com>
2014-12-01 21:06:20 -0200

    Responsive video loading

--- js/app.js
+++ js/app.js
@@ -6,13 +6,46 @@ WebFont.load({
 });
 
 $(document).ready(function() {
+
+  var videoAttached = false;
+
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+  var attachVideo = function() {
+    if(w >= 768) {
+      var video = document.createElement('video');
+
+      video.autoplay = true;
+      video.loop = true;
+
+      video.innerHTML = "<source src='videos/Bigu-Landing-Page.webm' type='video/webm'>" +
+        "<source src='videos/Bigu-Landing-Page.mp4' type='video/mp4'>";
+
+      $('body').prepend(video);  
+      videoAttached = true;
+    }
+  };
+
+
+  $(window).on('resize', function() {
+
+    w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+    attachVideo(); 
+  });
+
+    attachVideo(); 
+
+
   setTimeout(function() {
     $('header h1').show().css('animation', 'getIn 1500ms linear both');
     $('main').show().find('form').css('animation', 'fadeIn 1000ms linear both');
   }, 800);
 
 
-$('form').submit(function(ev) {
+  $('form').submit(function(ev) {
 
   ev.preventDefault();
 
@@ -27,7 +60,7 @@ $('form').submit(function(ev) {
         $form.removeClass('wobble');
       }, 1000);
     } else {
-       $('header, main').hide();
+       $('header, main').hide(1);
        $('.success').show().css('animation', 'fadeIn 1000ms linear both');
     }
   }, 'json');
